af22d787ca05d7541a47477604a2ed272bebb015
Thiago Wanderley <146901800+thiagowanderley@users.noreply.github.com>
2023-11-12 11:53:56 -0300

    Publicação do site

---
+++ newFile.js
@@ -0,0 +1,4 @@
+sr.reveal('.paragrafo', {
+    duration: 2000,
+    rotate: { x: 0, y: 80, z: 0 },
+});
---
+++ script.js
@@ -0,0 +1,17 @@
+
+window.sr = ScrollReveal({ reset:true });
+
+sr.reveal('.paragrafo', {
+    rotate: {x: 0, Y: 100, z: 0},
+    duration: 3000 
+});
+
+sr.reveal('.caixa-texto', {
+    rotate: {x: 0, Y: 100, z: 0},
+    duration: 2000 
+});
+
+sr.reveal('.pecas', {
+    rotate: {x: 0, Y: 100, z: 0},
+    duration: 1500 
+});
